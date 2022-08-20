6d4ff1a6d6fe4fb5c58c6988e49e10530fd826e0
mouse <mouse>
2022-08-20 10:52:39 -0700

    Add DP and Graphs

---
+++ python/dynamicProgramming/countPaths.py
@@ -0,0 +1,38 @@
+# Brute Force - Time and Space: O(2 ^ (n + m))
+def bruteForce(r, c, rows, cols):
+    if r == rows or c == cols:
+        return 0
+    if r == rows - 1 and c == cols - 1:
+        return 1
+    
+    return (bruteForce(r + 1, c, rows, cols) +  
+            bruteForce(r, c + 1, rows, cols))
+
+# Memoization - Time and Space: O(n * m)
+def memoization(r, c, rows, cols, cache):
+    if r == rows or c == cols:
+        return 0
+    if cache[r][c] > 0:
+        return cache[r][c]
+    if r == rows - 1 and c == cols - 1:
+        return 1
+    
+    cache[r][c] = (memoization(r + 1, c, rows, cols, cache) +  
+        memoization(r, c + 1, rows, cols, cache))
+    return cache[r][c]
+ 
+# Dynamic Programming - Time and Space: O(m), where m is num of cols
+def dp(rows, cols):
+    prevRow = [0] * cols
+
+    for r in range(rows - 1, -1, -1):
+        curRow = [0] * cols
+        curRow[cols - 1] = 1
+        for c in range(cols - 2, -1, -1):
+            curRow[c] = curRow[c + 1] + prevRow[c]
+        prevRow = curRow
+    return prevRow[0] 
+
+print(bruteForce(0, 0, 4, 4))
+print(memoization(0, 0, 4, 4, [[0] * 4 for i in range(4)]))
+print(dp(4, 4))
---
+++ python/dynamicProgramming/fibonacci.py
@@ -0,0 +1,32 @@
+# Brute Force
+def bruteForce(n):
+    if n <= 1:
+        return n
+    return bruteForce(n - 1) + bruteForce(n - 2)
+
+# Memoization
+def memoization(n, cache):
+    if n in cache:
+        return cache[n]
+
+    cache[n] = bruteForce(n - 1) + bruteForce(n - 2)
+    return cache[n]
+
+# Dynamic Programming
+def dp(n):
+    if n < 2:
+        return n
+
+    dp = [0, 1]
+    i = 2
+    while i <= n:
+        tmp = dp[1]
+        dp[1] = dp[0] + dp[1]
+        dp[0] = tmp
+        i += 1
+    return dp[1]
+
+
+print(bruteForce(10))
+print(memoization(10, {}))
+print(dp(10))
