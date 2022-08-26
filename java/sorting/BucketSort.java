b346d7febf29d7912002dfca444bf1d2e9ab10e1
ranveersingh2718 <ranveersingh10600@gmail.com>
2022-08-26 04:33:41 -0700

    3/4 sorting done

---
+++ java/sorting/BucketSort.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class BucketSort {
+
+    // Java implementation of Bucket Sort
+    public static int[] bucketSort(int[] arr) {
+        // Assuming arr only contains 0, 1 or 2
+        int[] counts = {0, 0, 0};
+
+        // Count the quantity of each val in arr
+        for (int num: arr) {
+            counts[num] += 1;
+        }
+
+        // Fill each bucket in the original array
+        int i = 0;
+        for (int n = 0; n < counts.length; n++) {
+            for (int j = 0; j < counts[n]; j++) {
+                arr[i] = n;
+                i++;
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 2, 0, 0, 2};
+        System.out.println(Arrays.toString(bucketSort(arr)));
+    }
+}
