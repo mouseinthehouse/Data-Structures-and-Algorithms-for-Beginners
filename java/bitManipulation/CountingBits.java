9eb372d969007bd2f8565158fa5024a8a9a1faeb
neetcode <neetcodebusiness@gmail.com>
2022-08-29 02:01:57 -0700

    java bitmanipulation

--- java/bitManipulation/CountingBits.java
+++ java/bitManipulation/CountingBits.java
@@ -1,5 +1,25 @@
 public class CountingBits {
-    
+    public static void operations() {
+        // AND
+        int n = 1 & 1;
+
+        // OR 
+        n = 1 | 0;
+
+        // XOR
+        n = 0 ^ 1;
+
+        // NOT (negation)
+        n = ~n;
+
+        // Bit Shifting
+        n = 1;
+        n = n << 1;
+        n = n >> 1;
+        
+        return;
+    }
+
     // Count the number of 1 bits in an int
     public static int countBits(int n) {
         int count = 0;
