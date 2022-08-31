32c38781d999f42368e0d1ecff1e469377628164
ranveersingh2718 <ranveersingh10600@gmail.com>
2022-08-31 03:17:04 -0700

    bfs printed levels wrong but traversed correctly

--- java/tree/BFS.java
+++ java/tree/BFS.java
@@ -18,26 +18,28 @@ import java.util.Deque;
 
 public class BFS {
     
-    public void bfs(TreeNode root) {
+    public void bfs(TreeNode root) { 
         Deque<TreeNode> queue = new ArrayDeque<TreeNode>();
         if (root != null) {
             queue.add(root);
-        }
-        
+        }    
         int level = 0;
-        while(queue.size() > 0) {
-            System.out.println("level: " + level);
-            for (int i = 0; i < queue.size(); i++) {
-                TreeNode curr = queue.removeFirst();
-                System.out.println(curr.val);
+        while(!queue.isEmpty()) {
+            System.out.print("level " + level + ": ");
+            int levelLength = queue.size();
+            for (int i = 0; i < levelLength; i++) {
+                TreeNode curr = queue.removeFirst(); 
+                System.out.print(curr.val + " ");
                 if(curr.left != null) {
-                    queue.add(curr.left);
+                    queue.add(curr.left);  
                 }
                 if(curr.right != null) {
                     queue.add(curr.right);
-                }
+                }  
             }
             level++;
+            System.out.println();
         }
+        
     }
 }
