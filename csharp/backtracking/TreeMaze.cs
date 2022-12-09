6a69c7ad858e0ed94927003600edc2c0313e19a3
Ketan Agnihotri <agnihotriketan@users.noreply.github.com>
2022-12-09 14:34:13 +0530

    backtracking added

---
+++ csharp/backtracking/TreeMaze.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeetCodeTester.backtracking
+{ 
+    /*  Definition of TreeNode:
+   public class TreeNode
+    {
+        public int Val { get; set; }
+        public TreeNode Left { get; set; }
+        public TreeNode Right { get; set; }
+
+        public TreeNode(int val)
+        {
+            this.Val = val;
+            this.Left = null;
+            this.Right = null;
+        }
+    }*/
+    public class TreeMaze
+    { 
+        public bool CanReachLeaf(TreeNode root)
+        {
+            if (root == null || root.Val == 0)
+            {
+                return false;
+            }
+            if (root.Left == null && root.Right == null)
+            {
+                return true;
+            }
+            if (CanReachLeaf(root.Left))
+            {
+                return true;
+            }
+            if (CanReachLeaf(root.Right))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool LeafPath(TreeNode root, List<int> path)
+        {
+            if (root == null || root.Val == 0)
+            {
+                return false;
+            }
+            path.Add(root.Val);
+
+            if (root.Left == null && root.Right == null)
+            {
+                return true;
+            }
+            if (LeafPath(root.Left, path))
+            {
+                return true;
+            }
+            if (LeafPath(root.Right, path))
+            {
+                return true;
+            }
+            path.Remove(path.Count - 1);
+            return false;
+        } 
+    } 
+}
---
+++ csharp/backtracking/TreeNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeetCodeTester.backtracking
+{
+    public class TreeNode
+    {
+        public int Val { get; set; }
+        public TreeNode Left { get; set; }
+        public TreeNode Right { get; set; }
+
+        public TreeNode(int val)
+        {
+            this.Val = val;
+            this.Left = null;
+            this.Right = null;
+        }
+    }
+
+}
