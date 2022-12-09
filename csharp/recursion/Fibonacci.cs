b66dfd6700db2f789a91ede040cb3f89e25e4ade
Ketan Agnihotri <agnihotriketan@users.noreply.github.com>
2022-12-09 14:27:33 +0530

    recursion

---
+++ csharp/recursion/Fibonacci.cs
@@ -0,0 +1,15 @@
+﻿public class Fibonacci
+{
+    // Recursive implementation to calculate the n-th Fibonacci number
+    // Base case: n = 0 or 1
+    public static int FibonacciSeries(int n)
+    {
+        if (n <= 1)
+        {
+            return n;
+        }
+
+        // Recursive case: fib(n) = fib(n - 1) + fib(n - 1)
+        return FibonacciSeries(n - 1) + FibonacciSeries(n - 2);
+    }
+}
