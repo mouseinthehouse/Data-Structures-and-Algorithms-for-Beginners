cfd3e4d9083274c5e3d68017f67771eb59e78f0a
Ketan Agnihotri <agnihotriketan@users.noreply.github.com>
2022-12-12 15:48:11 +0530

    formatting

--- csharp/backtracking/TreeMaze.cs
+++ csharp/backtracking/TreeMaze.cs
@@ -1,70 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*  Definition of TreeNode:
+public class TreeNode
+{
+    public int Val { get; set; }
+    public TreeNode Left { get; set; }
+    public TreeNode Right { get; set; }
 
-namespace NeetCodeTester.backtracking
-{ 
-    /*  Definition of TreeNode:
-   public class TreeNode
+    public TreeNode(int val)
     {
-        public int Val { get; set; }
-        public TreeNode Left { get; set; }
-        public TreeNode Right { get; set; }
-
-        public TreeNode(int val)
+        this.Val = val;
+        this.Left = null;
+        this.Right = null;
+    }
+}*/
+public class TreeMaze
+{ 
+    public bool CanReachLeaf(TreeNode root)
+    {
+        if (root == null || root.Val == 0)
         {
-            this.Val = val;
-            this.Left = null;
-            this.Right = null;
-        }
-    }*/
-    public class TreeMaze
-    { 
-        public bool CanReachLeaf(TreeNode root)
-        {
-            if (root == null || root.Val == 0)
-            {
-                return false;
-            }
-            if (root.Left == null && root.Right == null)
-            {
-                return true;
-            }
-            if (CanReachLeaf(root.Left))
-            {
-                return true;
-            }
-            if (CanReachLeaf(root.Right))
-            {
-                return true;
-            }
             return false;
         }
-
-        public bool LeafPath(TreeNode root, List<int> path)
+        if (root.Left == null && root.Right == null)
         {
-            if (root == null || root.Val == 0)
-            {
-                return false;
-            }
-            path.Add(root.Val);
+            return true;
+        }
+        if (CanReachLeaf(root.Left))
+        {
+            return true;
+        }
+        if (CanReachLeaf(root.Right))
+        {
+            return true;
+        }
+        return false;
+    }
 
-            if (root.Left == null && root.Right == null)
-            {
-                return true;
-            }
-            if (LeafPath(root.Left, path))
-            {
-                return true;
-            }
-            if (LeafPath(root.Right, path))
-            {
-                return true;
-            }
-            path.Remove(path.Count - 1);
+    public bool LeafPath(TreeNode root, List<int> path)
+    {
+        if (root == null || root.Val == 0)
+        {
             return false;
-        } 
+        }
+        path.Add(root.Val);
+
+        if (root.Left == null && root.Right == null)
+        {
+            return true;
+        }
+        if (LeafPath(root.Left, path))
+        {
+            return true;
+        }
+        if (LeafPath(root.Right, path))
+        {
+            return true;
+        }
+        path.Remove(path.Count - 1);
+        return false;
     } 
-}
+} 
+ 
