176125419934cecd4ab42abb3e99832fd3c0552a
Ketan Agnihotri <agnihotriketan@users.noreply.github.com>
2022-12-12 17:26:14 +0530

    formatted

--- csharp/backtracking/TreeNode.cs
+++ csharp/backtracking/TreeNode.cs
@@ -1,23 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NeetCodeTester.backtracking
+﻿public class TreeNode
 {
-    public class TreeNode
+    public int Val { get; set; }
+    public TreeNode Left { get; set; }
+    public TreeNode Right { get; set; }
+
+    public TreeNode(int val)
     {
-        public int Val { get; set; }
-        public TreeNode Left { get; set; }
-        public TreeNode Right { get; set; }
-
-        public TreeNode(int val)
-        {
-            this.Val = val;
-            this.Left = null;
-            this.Right = null;
-        }
+        this.Val = val;
+        this.Left = null;
+        this.Right = null;
     }
-
 }
